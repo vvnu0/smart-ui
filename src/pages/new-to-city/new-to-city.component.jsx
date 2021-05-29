227a1278a8164445f70086d2ff37709761874216
Vishnu Nair <nairvishnumail@gmail.com>
2021-05-29 13:56:14 -0700

    Added new to city links

--- src/pages/new-to-city/new-to-city.component.jsx
+++ src/pages/new-to-city/new-to-city.component.jsx
@@ -13,9 +13,21 @@ const NewToCity = () => {
       <h1> Welcome, {user.userDetails.displayName}</h1>
       <h2> Apply For : </h2>
       <ul>
-        <li>PGE</li>
-        <li>Water</li>
-        <li>Trash</li>
+        <li>
+          <label onClick={() => {window.location.assign("https://m.pge.com/#startorstopservice");}} target="_blank">
+            PGE
+          </label>
+        </li>
+        <li>
+          <label onClick={() => {window.location.assign("https://www.acwd.org/117/Starting-Stopping-Service");}}>
+            Water
+          </label>
+        </li> 
+        <li>
+          <label onClick={() => {window.location.assign("https://www.republicservices.com/municipality/fremont-ca");}}>
+            Trash
+          </label>
+        </li>
         <li>Free City Wifi</li>
         <li>Unified Payment Paltform</li>
       </ul>
