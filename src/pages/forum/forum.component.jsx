665a6123a820c899f965f1fdfe65d061974b0bcd
Vishnu Nair <nairvishnumail@gmail.com>
2021-05-29 23:39:57 -0700

    forum updated

--- src/pages/forum/forum.component.jsx
+++ src/pages/forum/forum.component.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import postComment from './postcomment.png';
 const Forum = () => {
   return (
     <div>
-      <h1>Discussion Forum on city issues/concerns</h1>
-      <h2>Coming Soon</h2>
+      <h2>Discussion forum on city issues</h2>
+	  <h3>Adding suggestions, upvoting suggestions </h3>
+      <h4>Coming Soon</h4>
+	  <div>
+          <img src={postComment} alt="City Issues"/>
+       </div>
     </div>
   );
 };
